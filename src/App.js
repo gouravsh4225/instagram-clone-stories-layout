e89ee67fb4f432dc84aee17f7964db6878c62981
gouravsh4225 <gouravsh4225@gmail.com>
2020-10-24 21:48:56 +0530

    Change professional email to personal email for commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <React.Fragment>Hello</React.Fragment>;
+  return <React.Fragment>Hello Instgram Web App</React.Fragment>;
 }
 
 export default App;
