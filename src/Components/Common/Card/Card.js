55965b38ce2cdf1ce587ef45bf61dfea6c61aba0
gouravsh4225 <gouravsh4225@gmail.com>
2020-10-25 16:23:27 +0530

    add stories view modal,add user post

--- src/Components/Common/Card/Card.js
+++ src/Components/Common/Card/Card.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-function Card() {
-  return <div>Card</div>;
+import "./Card.css";
+
+function Card(props) {
+  console.log(props, "props");
+  return (
+    <div className="post-card">
+      <div className="post__header">
+        <div className="post__user">
+          <img src={props.avatar} className="post__avatar" />
+          <div className="post__username">
+            {`${props.first_name} ${props.last_name}`}
+            <div className="post__email"> {props.email}</div>
+          </div>
+        </div>
+      </div>
+      <div className="post__content">
+        {props?.post?.type.toLowerCase() === "image" && (
+          <img src={props?.post?.post_url} className="content__image" />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/Components/StoriesList/StoryViewModal/ModalViewOverlay.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function ModalViewOverlay(props) {
-  return <div className="modal-overlay"></div>;
-}
-export default ModalViewOverlay;
